508b3773a6a72d627b275a87c811f0901846e4d8
westy02 <westy02@gmail.com>
2013-03-01 22:06:52 +1300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GEFL::Application.config.secret_token = '467f56f8749f061b4d6cec30dbff947082e2405a7199b133f3dded048a5841164c9213ee3fbad1a3c790c0a317666b6f8c383347d63ce3dfee9fb43b679afe81'
