f150c01bd6306e9bfc50ebeed4db7542d49553d6
westy02 <westy02@gmail.com>
2013-03-25 21:58:41 +1300

    set s3 fog heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,6 +9,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
    include Sprockets::Helpers::RailsHelper
    include Sprockets::Helpers::IsolatedHelper
+   
+   include CarrierWave::MimeTypes
+  process :set_content_type
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
