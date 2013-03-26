e4e1c9427aae92665a48b729c6986a745840b0fa
westy02 <westy02@gmail.com>
2013-03-26 22:22:46 +1300

    add security to admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
  
+ http_basic_authenticate_with :name => ENV["ADMIN_NAME"], :password => ENV["ADMIN_PASS"], :only => [:admin]
+ 
   def index
     @title = "Home"
     @categories = Category.order("id asc")
--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,7 @@
 class ServicesController < ApplicationController
+  
+  http_basic_authenticate_with :name => ENV["ADMIN_NAME"], :password => ENV["ADMIN_PASS"], :except => [:show]
+  
   # GET /services
   # GET /services.json
   def index
