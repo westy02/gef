780015c32f3025ccd892decbdf69df3d6debe615
westy02 <westy02@gmail.com>
2013-03-26 21:06:46 +1300

    add image magick and resize thumbnails

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -32,11 +32,11 @@ class ImageUploader < CarrierWave::Uploader::Base
    end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+   process :resize_to_limit => [675, 738]
   #
   # def scale(width, height)
   #   # do something
-  # end
+   end
 
   # Create different versions of your uploaded files:
    version :thumb do
