0b3d3282e5d1f6e317c570637bbac9123507060d
westy02 <westy02@gmail.com>
2013-03-27 20:33:08 +1300

    add links to thumbnails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,24 +5,10 @@ class PagesController < ApplicationController
   def index
     @title = "Home"
     @categories = Category.order("id asc")
-  end
-  
-  def concrete
-   
-  end
-  
-  def timber
-   
-  end
-  
-  def vinyl
-   
-  end
-  
-  def carpet
-   
-  end
 
+  end
+  
+ 
   def about  
    
   end
