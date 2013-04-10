b51e3adba2ca168723e3725fdcbeef323047aa35
westy02 <westy02@gmail.com>
2013-04-10 21:04:43 +1200

    add active admin gem

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,7 @@ module GEFL
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css )
   end
 end
